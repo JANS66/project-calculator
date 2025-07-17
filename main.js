4ac09700389a42b8a0cc52a3316160b053962a84
JANS <98973687+JANS66@users.noreply.github.com>
2025-07-17 19:13:09 +0300

    feat: Calculator will functionate properly

--- main.js
+++ main.js
@@ -26,30 +26,69 @@ function operate(num1, operator, num2) {
     };
 };
 
+const buttons = document.querySelectorAll("button");
+let disableOperators = true;
+let disableEquals = true;
+
+function toggleOperators(disableOperators, disableEquals) {
+    if (disableOperators) {
+        buttons.forEach(button => {
+            if (["+", "-", "/", "*"].includes(button.textContent)) {
+                button.disabled = true;
+            }
+        })
+    } else if (!disableOperators) {
+        buttons.forEach(button => {
+            if (["+", "-", "/", "*"].includes(button.textContent)) {
+                button.disabled = false;
+            }
+        })
+    }
+
+    if (disableEquals) {
+        buttons.forEach(button => {
+            if (["="].includes(button.textContent)) {
+                button.disabled = true;
+            }
+        })
+    } else if (!disableEquals) {
+        buttons.forEach(button => {
+            if (["="].includes(button.textContent)) {
+                button.disabled = false;
+            }
+        })
+    }
+}
+
 function populateDisplay() {
-    const buttons = document.querySelectorAll("button");
     const display = document.querySelector("#display");
+    toggleOperators(disableOperators, disableEquals);
 
     buttons.forEach(button => {
         button.addEventListener("click", (e) => {
             display.textContent += e.target.textContent;
 
-            // Disable operators after one operator chosen
-            if (/[+\-*/]/.test(e.target.textContent)) {
-                buttons.forEach(button => {
-                    if (["+", "-", "/", "*"].includes(button.textContent)) {
-                        button.disabled = true;
-                    }
-                });
+            toggleOperators(disableOperators = false, disableEquals = true);
+
+            if (/[+\-*/]/.test(display.textContent)) {
+                toggleOperators(disableOperators = true, disableEquals = true);
+            } 
+
+            if(/[0-9]/.test(e.target.textContent)) {
+                const lastOperatorIndex = Math.max(
+                    display.textContent.lastIndexOf("+"),
+                    display.textContent.lastIndexOf("-"),
+                    display.textContent.lastIndexOf("*"),
+                    display.textContent.lastIndexOf("/")
+                );
+
+                if (lastOperatorIndex !== -1) {
+                    toggleOperators(disableOperators = true, disableEquals = false);
+                }
             }
 
             // Slice everything into variables and calculate
             if (e.target.textContent === "=") {
-                buttons.forEach(button => {
-                    if ("=".includes(button.textContent)) {
-                        button.disabled = true;
-                    }
-                })
                 // Find operator in display
                 const operators = ["+", "-", "*", "/"];
                 // Find first operator
@@ -66,11 +105,7 @@ function populateDisplay() {
             // Clear everything and enable operators 
             if (e.target.textContent === "CLR") {
                 display.textContent = "";
-                buttons.forEach(button => {
-                    if (["+", "-", "/", "*", "="].includes(button.textContent)) {
-                        button.disabled = false;
-                    }
-                })
+                toggleOperators(disableOperators = true, disableEquals = true);
             }
         })
     });
